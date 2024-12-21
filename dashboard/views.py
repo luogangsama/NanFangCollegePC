2641e68d1761faf9e4defe3b10d2ec23dbe86e5d
luoenhao <3070845578@qq.com>
2024-12-21 20:48:23 +0800

    add a table: call_report

--- dashboard/views.py
+++ dashboard/views.py
@@ -20,7 +20,22 @@ def modifyPassword(request):
 def get_weather(request):
     apiKey = '7be7dff3729983328f5bbc4815cd5022'
     sessionid = request.COOKIES.get('sessionid')
-    
+
+    if sessionid:
+        try:
+            session = Session.objects.get(session_key=sessionid)
+            if session.expire_date > timezone.now():
+                return JsonResponse({'message': 'Success', 'apiKey': apiKey}, status=200)
+            else:
+                return JsonResponse({'message': 'Session has expired'}, status=200)
+        except Session.DoesNotExist:
+            return JsonResponse({'message': 'Invalid session'}, status=200)
+    else:
+        return JsonResponse({'message': 'No sessionid cookie'}, status=200)
+
+def customer_report(request):
+    sessionid = request.COOKIES.get('sessionid')
+
     if sessionid:
         try:
             session = Session.objects.get(session_key=sessionid)
--- login/views.py
+++ login/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+from django.utils.timezone import now
 from django.http import JsonResponse
 import json
 import hashlib
@@ -35,3 +37,31 @@ def signin(request):
         else:
             response = Response(message='PASSWORD ERROR', method='POST')
             return response
+
+def get_user_from_sessionid(sessionid):
+    try:
+        # 获取会话对象
+        session = Session.objects.get(session_key=sessionid)
+
+        # 检查会话是否过期
+        if session.expire_date < now():
+            return None  # 会话已过期
+
+        # 获取会话数据
+        session_data = session.get_decoded()
+
+        # 提取用户 ID
+        user_id = session_data.get('_auth_user_id')
+
+        # 返回用户对象
+        return User.objects.get(pk=user_id) if user_id else None
+    except Session.DoesNotExist:
+        return None  # sessionid 无效
+    except User.DoesNotExist:
+        return None  # 用户不存在
+
+def val_is_sigined(request):
+    sessionid = request.COOKIES['sessionid']
+    user = get_user_from_sessionid(sessionid)
+
+    return JsonResponse({'message': 'Success', 'isValid': 'true'})
