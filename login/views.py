7e76a7d3c756cc3ad0fd543f100991567a74086c
luoenhao <3070845578@qq.com>
2024-12-23 17:31:17 +0800

    引入自动登录所需的模块timezone与now

--- login/views.py
+++ login/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
+from django.utils import timezone
+from django.utils.timezone import now
 from django.http import JsonResponse
 import json
 import hashlib
