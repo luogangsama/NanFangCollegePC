301933ecba0270a7e005e9d6db032850cd366950
luoenhao <3070845578@qq.com>
2024-12-25 17:37:01 +0800

    delete old modify password api

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from dashboard.views import modifyPassword
 from dashboard.views import get_weather
 from dashboard.views import call_report
 from dashboard.views import user_get_history_report
