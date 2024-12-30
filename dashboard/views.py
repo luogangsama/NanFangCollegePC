4c5fbdb59782c3c34f7ae5054a1bc345488454cc
luoenhao <3070845578@qq.com>
2024-12-31 00:06:15 +0800

    修改错误

--- dashboard/views.py
+++ dashboard/views.py
@@ -531,7 +531,7 @@ def get_staff_of_same_day(request):
 
                 for staff in staffs:
                     return_data['workers'].append({'username': staff.user.username})
-                JsonResponse(return_data, status=200)
+                return JsonResponse(return_data, status=200)
             else:
                 return JsonResponse({'message': 'Session has expired'}, status=200)
         except Session.DoesNotExist:
