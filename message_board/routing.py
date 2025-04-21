12ab6db4230d86bf8ea04f2d7a36ebaf79821a30
luoenhao <3070845578@qq.com>
2025-04-21 13:40:04 +0800

    websocket配置中

--- message_board/routing.py
+++ message_board/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
-from . import consumers # 类似于views.py功能的文件，需要新建
+from message_board import consumers # 类似于views.py功能的文件，需要新建
 
 websocket_urlpatterns = [
-    re_path(r'/ws/message/(?P<user_id>\w+)/(?P<report_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/message/(?P<user_id>\d+)/(?P<report_id>\d+)/$', consumers.MessageConsumer.as_asgi()),
 ]
