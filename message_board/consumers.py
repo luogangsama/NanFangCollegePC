efaf9ee002141d9b3424c9c91d3af2fdc9a37cf8
luoenhao <3070845578@qq.com>
2025-04-22 11:44:52 +0800

    取消username前缀

--- message_board/consumers.py
+++ message_board/consumers.py
@@ -16,12 +16,7 @@ class MessageConsumer(AsyncWebsocketConsumer):
             return
 
         self.user = user
-
-
-        if user.last_name != 'customer':
-            self.username = f'志愿者-{user.username}'
-        else:
-            self.username = user.username
+        self.username = user.username
         self.room_group_name = f'message_{self.report_id}'
 
         # 加入房间组
