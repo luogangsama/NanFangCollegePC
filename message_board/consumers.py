3939fff69ddecc9defbb6ee6f029f8543984277b
luoenhao <3070845578@qq.com>
2025-04-27 09:39:15 +0800

    修改留言板数据库查询语句

--- message_board/consumers.py
+++ message_board/consumers.py
@@ -61,7 +61,7 @@ class MessageConsumer(AsyncWebsocketConsumer):
         }))
         reportId = self.report_id
         # 根据订单号获取订单对象
-        report = call_report_table.objects.get(pk=reportId)
+        report = call_report_table.objects.get(id=reportId)
         # 根据订单对象往留言记录表中插入记录
         report_message_board_record.objects.create(
             report=report,
