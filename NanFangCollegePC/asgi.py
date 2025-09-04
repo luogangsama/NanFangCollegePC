5996101cb2f45a0fb632af3e73822f47cb2d29c3
luoenhao <3070845578@qq.com>
2025-09-04 18:58:04 +0800

    使用Dephne作为项目的asgi服务器

--- NanFangCollegePC/asgi.py
+++ NanFangCollegePC/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NanFangCollegePC.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from message_board import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NanFangCollegePC.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
