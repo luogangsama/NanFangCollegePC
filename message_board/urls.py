39142717b03bbdfd32d4cb82c7f33e39de7b1b18
luoenhao <2332893844@qq.com>
2025-09-08 16:37:07 +0800

    为业务代码增加路由

--- message_board/urls.py
+++ message_board/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from message_board.views import get_message_record
+from message_board.views import get_message_list
 
 urlpatterns = [
-    path('get_message_record/', get_message_record)
+    path('get_message_record/', get_message_record),
+    path('get_message_list/'. get_message_list)
 ]
