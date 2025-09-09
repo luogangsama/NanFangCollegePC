3f9447fff57a13f4268d1fd921854841d8fb5f7f
luoenhao <2332893844@qq.com>
2025-09-09 15:28:05 +0800

    添加django静态资源路径，并修复部分错误

--- NanFangCollegePC/settings.py
+++ NanFangCollegePC/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -226,3 +227,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     # a['b']
 # except Exception as e:
     # logger.opt(exception=True).error(f"错误信息: {e}")
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
