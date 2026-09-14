cdc2c15915b08b1e59588a32b0c28094690301ed
luoenhao <3070845578@qq.com>
2026-09-14 23:06:12 +0800

    1

--- NanFangCollegePC/settings.py
+++ NanFangCollegePC/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+env = eniron.Env()
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
